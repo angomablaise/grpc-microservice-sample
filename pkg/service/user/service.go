8ce7d279d0fd75d0dd856362001c2a8292aa9151
Takahiro Oshima <tarotora51@gmail.com>
2019-05-05 21:33:05 +0900

    user service完成

--- pkg/service/user/service.go
+++ pkg/service/user/service.go
@@ -107,3 +107,55 @@ func (s *server) Update(ctx context.Context, req *api.UpdateUserRequest) (*api.U
 
 	return &api.UpdateUserResponse{Updated: rows}, nil
 }
+
+func (s *server) Delete(ctx context.Context, req *api.DeleteUserRequest) (*api.DeleteUserResponse, error) {
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	res, err := c.ExecContext(ctx, "DELETE FROM users WHERE `id`=?", req.Id)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to delete "+err.Error())
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return nil, status.Error(codes.Unknown, err.Error())
+	}
+
+	if rows == 0 {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("ID='%d' is not found",
+			req.Id))
+	}
+	return &api.DeleteUserResponse{Deleted: rows}, nil
+}
+
+func (s *server) GetAll(ctx context.Context, req *api.GetAllUserRequest) (*api.GetAllUserResponse, error) {
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	rows, err := c.QueryContext(ctx, "SELECT `id`, `name`, `age`, `mail`, `address` FROM users")
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to select "+err.Error())
+	}
+	defer rows.Close()
+
+	list := []*api.User{}
+	for rows.Next() {
+		user := new(api.User)
+		if err := rows.Scan(&user.Id, &user.Name, &user.Age, &user.Mail, &user.Address); err != nil {
+			return nil, status.Error(codes.Unknown, err.Error())
+		}
+		list = append(list, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, status.Error(codes.Unknown, err.Error())
+	}
+	return &api.GetAllUserResponse{Users: list}, nil
+}
